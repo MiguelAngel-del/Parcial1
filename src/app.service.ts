f4ce12197ca331be5c9fdbb3a5b27c15591d205d
MiguelAngel-del <miguelgarcia@umes.edu.gt>
2025-07-25 12:23:21 -0600

    Cambio de Miguel

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola Mundo Cambio!';
+    return 'Hola Mundo Cambio a español!';
   }
 }
