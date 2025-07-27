15ac1c0237cf462c041909748f1fba37e0a494a3
DSamuelQ <samuelquiji@gmail.com>
2025-07-27 00:25:09 -0600

    CRUD para los roles de usuarios

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,1 +1,14 @@
-export class CreateRoleDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsString, MinLength } from 'class-validator';
+
+export class CreateRoleDto {
+  @ApiProperty({                    //Esto es lo que se agrega para los endpoints de Swagger
+    description: 'Nombre del rol',  // Una pequeña descripción del campo
+    example: 'Administrador',       // Se pueden poner ejemplos para los campos
+  })
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  @MinLength(1)
+  nombreRol: string;
+}
